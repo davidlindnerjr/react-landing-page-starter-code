ee1c10455624fcb4bff5faeb38845ede15dc5397
David Lindner <davidlindnerjr@gmail.com>
2021-02-06 18:00:45 -0800

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,96 @@
+const reignCans = [
+
+    {
+        name: "Tru Blu",
+        image: "/images/tru-blu.png",
+        description: "Revolutionary in it's function, classic in it's taste. REIGN'S Tru Blu delivers that traditional energy drink flavor with a dose of metabolism boosting.",
+        series: "Inferno",
+        quantity: 1
+    },
+    {
+        name: "Strawberry Jalepeno",
+        image: "/images/strawberry-jalepeno.png",
+        description: "A delightful mix of sweet berry with a hint of heat topped with thermogenic fuel to help you through any workout.",
+        series: "Inferno",
+        quantity: 1
+    },
+    {
+        name: "Red Dragon",
+        image: "/images/red-dragon.webp",
+        description: "Fruit punch flavor with a kick! 300 mg of caffeine, 10 calories, 0 sugar.",
+        series: "Inferno",
+        quantity: 1
+    },
+    {
+        name: "Lilikoi Lychee",
+        image: "/images/lilikoi-lychee.png",
+        description: "Tropical power flavor! Our passion fruit inspired flavor with a sensation that'll have your taste buds island bound.",
+        series: "Origianl",
+        quantity: 1
+    },
+    {
+        name: "Melon Mania",
+        image: "/images/melon-mania.png",
+        description: "A summer staple year-round. Mouthwatering refreshment in it's finest form.",
+        series: "Original",
+        quantity: 1
+    },
+    {
+        name: "Sour Apple",
+        image: "/images/sour-apple.png",
+        description: "A classic blend of sweet and sour in REIGN's Sour Apple will have you quaking.",
+        series: "Original",
+        quantity: 1
+    },
+    {
+        name: "Carnival Candy",
+        image: "/images/carnival-candy.webp",
+        description: "Throwback to your summers at the state fair. Carnival Candy delivers that sweet taste of cotton candy with that caffeine kick.",
+        series: "Original",
+        quantity: 1
+    },
+    {
+        name: "Razzle Berry",
+        image: "/images/razzle-berry.png",
+        description: "This is how you do a berry flavor! A flawless mixture of blueberries, blackberries, and raspberries.",
+        series: "Original",
+        quantity: 1
+    },
+    {
+        name: "Orange Dreamsicle",
+        image: "/images/orange-dream.webp",
+        description: "The perfect blend of orange and vanilla. The perfect frozen treat inspired energy drink to fuel your workout.",
+        series: "Original",
+        quantity: 1
+    },
+    {
+        name: "Strawberry Sublime",
+        image: "/images/strawberry-sublime.webp",
+        description: "The three bffs strawberry, guava, and lime here to leave you quenched and fueled for your training session.",
+        series: "Original",
+        quantity: 1
+    },
+    {
+        name: "Mango-O-Matic",
+        image: "/images/mango-matic.webp",
+        description: "Mango flavor to bring the tropics to any of your workouts.",
+        series: "Original",
+        quantity: 1
+    },
+    {
+        name: "Lemon Hdz",
+        image: "/images/lemon-hdz.webp",
+        description: "The flavor of classic lemon with zero sugar and only 10 calories.",
+        series: "Original",
+        quantity: 1
+    },
+    {
+        name: "Peach Fizz",
+        image: "/images/peach-fizz.webp",
+        description: "Catch those sweet Georgia peach flavor in this fizzy refresher.",
+        series: "Original",
+        quantity: 1
+    }
+]
+
+export default reignCans;
